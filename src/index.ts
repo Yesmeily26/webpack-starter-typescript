d93d76caaba9414b8d8d57c73ff78c5729a67cf6
Yesmeily Cabreja Maria <yesmeilycabrejamaria026@gmail.com>
2021-04-05 19:24:04 -0500

    orderBy

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,11 @@
 import db from './firebase/config';
 import { retornaDocumentos } from './helpers/mostrar-documentos';
-const usuario = {
-nombre:'yesmeily',
-activo:true,
-fechaNaci:0
-}
+
 const usuariosRef = db.collection('usuarios');
-// db.collection('usuarios')
-// .add(usuario)
-// .then( docRef => {
-// console.log( docRef )
-// })
-// .catch( e => console.log('error, e '))
 
 
-// usuariosRef
-// .doc('8Yuve3P1Z6AjXEUb6nLW')
-// .update({
-//  activo: true   
-// });
+usuariosRef
+.orderBy('nombre')
+.orderBy('salario')
+.get().then( retornaDocumentos);
 
-// usuariosRef
-// .doc('8Yuve3P1Z6AjXEUb6nLW')
-// .set({
-//  activo: true,  
-//   edad:17,
-// });
-// usuariosRef 
-// .doc('8Yuve3P1Z6AjXEUb6nLW')
-// .delete()
-// .then( () => console.log('borrado') )
-// .catch( e => console.log('error', e));
-
-// usuariosRef
-// .onSnapshot(retornaDocumentos)
-   
-// usuariosRef.get().then( retornaDocumentos );
-
-// usuariosRef.where('salario', '>', 1800 )
-// .get().then( retornaDocumentos );
-
-// usuariosRef.where('salario', '>=', 1800 )
-// .where('salario', '<=', 2300)
-// .get().then( retornaDocumentos);
-
-// usuariosRef.where('salario', '>=', 1800 )
-// .where('activo', '==', true )
-// .get().then( retornaDocumentos);
